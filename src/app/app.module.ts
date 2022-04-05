8f411a755fdc97e565ea73f5203af258ef465081
ionovnn <ionovnn@monqlab.com>
2022-04-05 19:21:59 +0300

    All files

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CountryService } from './country.service';
+import { HomeComponent } from './home/home.component';
+import { AllCountriesComponent } from './all-countries/all-countries.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component'
+
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AllCountriesComponent,
+    CountryDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [CountryService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
